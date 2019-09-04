0078b986e60c685a80a3e13e7008bd54aafcbc7f
Benjamin Leedy <benjamin.leedy@gmail.com>
2019-09-04 13:45:55 -0500

    The rocket moves!

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,7 +23,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Space Pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
