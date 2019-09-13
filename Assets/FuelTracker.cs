04a584208951f090f9ae969725ac6707e8b5fbd7
Benjamin Leedy <benjamin.leedy@gmail.com>
2019-09-13 06:41:34 -0500

    Working on fuel

---
+++ Assets/FuelTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelTracker : MonoBehaviour
+{
+    [SerializeField] GameObject rocketShip;
+    float fuelLevel = rocketShip.fuelLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localScale.x = fuelLevel;
+    }
+}
