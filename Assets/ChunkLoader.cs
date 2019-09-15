8da9e8c0df48a241974436cb42a382ac036d37b8
Benjamin Leedy <benjamin.leedy@gmail.com>
2019-09-14 20:04:09 -0500

    prep to load chunks

---
+++ Assets/ChunkLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkLoader : MonoBehaviour
+{
+    [SerializeField] GameObject playerRocket;
+    float loadLine = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loadLine++;
+        print(loadLine);
+        if (playerRocket.transform.position.y > loadLine)
+        {
+            LoadChunk();
+        }
+    }
+
+    void LoadChunk()
+    {
+        loadLine++;
+    }
+}
