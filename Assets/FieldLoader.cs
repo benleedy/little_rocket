da515f5513929c6f3fb630312db3982ed36027b5
Benjamin Leedy <benjamin.leedy@gmail.com>
2019-09-16 23:21:05 -0500

    Almost ready to load chunks when the player lands

--- Assets/FieldLoader.cs
+++ Assets/FieldLoader.cs
@@ -4,21 +4,64 @@ using UnityEngine;
 
 public class FieldLoader : MonoBehaviour
 {
-    [SerializeField] GameObject asteroidPattern;
+    [SerializeField] GameObject playerRocket;
+    float loadLine = 60f;
+
+    [SerializeField] GameObject landingZone;
+    [SerializeField] GameObject asteroidPattern00;
+    [SerializeField] GameObject asteroidPattern01;
+    [SerializeField] GameObject asteroidPattern02;
+    [SerializeField] GameObject asteroidPattern03;
+    [SerializeField] GameObject asteroidPattern04;
+    [SerializeField] GameObject asteroidPattern05;
+    [SerializeField] GameObject asteroidPattern06;
+    [SerializeField] GameObject asteroidPattern07;
+    [SerializeField] GameObject asteroidPattern08;
+    [SerializeField] GameObject asteroidPattern09;
+
+    GameObject[] fields = new GameObject[10];
+
     // Start is called before the first frame update
     void Start()
     {
-        LoadAsteroids();
+        fields[0] = asteroidPattern00;
+        fields[1] = asteroidPattern01;
+        fields[2] = asteroidPattern02;
+        fields[3] = asteroidPattern03;
+        fields[4] = asteroidPattern04;
+        fields[5] = asteroidPattern05;
+        fields[6] = asteroidPattern06;
+        fields[7] = asteroidPattern07;
+        fields[8] = asteroidPattern08;
+        fields[9] = asteroidPattern09;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //when the player lands on a checkpoint, trigger loading the next chunk
+        if (layerRocket.landed == true)
+        {
 
+        for (int i = 0; i < 4; i++)
+            {
+                if (i == 2)
+                {
+                    Instantiate(landingZone, transform.position, transform.rotation);
+                }
+                else
+                {
+
+                LoadAsteroids(i);
+                }
+            }
+        }
     }
 
-    void LoadAsteroids()
+    void LoadAsteroids(int pattern)
     {
-        Instantiate(asteroidPattern, transform.position, transform.rotation);
+        Instantiate(fields[pattern], transform.position, transform.rotation);
     }
 }
