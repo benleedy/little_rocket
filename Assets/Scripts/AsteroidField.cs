d12285f2936cb1ed9a578cf09592e770962f1a70
Benjamin Leedy <benjamin.leedy@gmail.com>
2020-09-04 22:01:08 -0500

    Prototyping Zero G

---
+++ Assets/Scripts/AsteroidField.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidField : MonoBehaviour
+{
+    [SerializeField] GameObject asteroid;
+    [SerializeField] GameObject asteroidField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(asteroid, new Vector3(0, 10, 0), Quaternion.Euler(0,0,0), asteroidField.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FolloowCamZeroG.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolloowCamZeroG : MonoBehaviour
+{
+    [SerializeField] Transform playerRocketTransform;
+    //float cameraDistance = 9;
+    [SerializeField] Vector3 cameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (playerRocketTransform.position.y <= 21f)
+        //{
+        //    transform.position = new Vector3(0f, 21f, -82f);
+        //}
+        //else
+        //{
+            transform.position = new Vector3(playerRocketTransform.position.x, playerRocketTransform.position.y, -82f);
+        //}
+
+        /*
+        transform.position = new Vector3(0f, playerRocketTransform.position.y, -82f);
+        print(transform.position);
+
+    
+        */
+    }
+}
